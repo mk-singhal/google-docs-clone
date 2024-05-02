7eb552e55c7f0cf896e382bf84f728380ef7583f
mk-singhal <maniksingal29@gmail.com>
2024-05-02 14:23:40 +0530

    unique id & socket-connection for docs

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import TextEditor from "./TextEditor";
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   return (
-    <div className="App">
-      <TextEditor />
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          exact
+          element={<Navigate to={`/documents/${uuidv4()}`} />}
+        />
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+    </Router>
   );
 }
 
